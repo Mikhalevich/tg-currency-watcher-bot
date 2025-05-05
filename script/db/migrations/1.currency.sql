4ebec291d061a71847e0f91d269455d38abe6283
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-05-05 16:57:16 +0300

    currency table

--- script/db/migrations/1.currency.sql
+++ script/db/migrations/1.currency.sql
@@ -1,5 +1,23 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
+CREATE TABLE currency(
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    base TEXT NOT NULL,
+    base_external_id TEXT NOT NULL,
+    quote TEXT NOT NULL,
+    quote_external_id TEXT NOT NULL,
+    price DECIMAL NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE UNIQUE INDEX base_ext_id_quote_ext_id_idx ON currency(base_external_id, quote_external_id);
+
+INSERT INTO currency(base, base_external_id, quote, quote_external_id, price, updated_at) VALUES
+    ('USD', '2781', 'BTC', '1', 0, CURRENT_TIMESTAMP);
+
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
+
+DROP INDEX base_ext_id_quote_ext_id_idx;
+DROP TABLE currency;
