89df808d01e1bf694e562e7aa29898081e301b84
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-05-16 18:02:38 +0300

    update currencies query updaed

--- internal/adapter/storage/postgres/update_currencies.go
+++ internal/adapter/storage/postgres/update_currencies.go
@@ -2,49 +2,71 @@ package postgres
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"strings"
 
-	"github.com/ericlagergren/decimal"
-	"github.com/volatiletech/sqlboiler/v4/boil"
-	"github.com/volatiletech/sqlboiler/v4/types"
-
-	"github.com/Mikhalevich/tg-currency-watcher-bot/internal/adapter/storage/postgres/internal/models"
 	"github.com/Mikhalevich/tg-currency-watcher-bot/internal/domain/exchange"
 )
 
-func (p *Postgres) UpdateCurrencies(ctx context.Context, currencies []exchange.Currency) error {
-	dbCurrencies := toDBCurrencies(currencies)
+const (
+	insertCurrencyColumns = 7
+)
 
-	for _, v := range dbCurrencies {
-		if _, err := v.Update(ctx, p.db, boil.Infer()); err != nil {
-			return fmt.Errorf("update row: %w", err)
-		}
+func (p *Postgres) UpdateCurrencies(ctx context.Context, currencies []exchange.Currency) error {
+	var (
+		queryTemplate = `
+			INSERT INTO currency(
+				base,
+				base_external_id,
+				quote,
+				quote_external_id,
+				price,
+				is_inverted,
+				updated_at
+			) VALUES %s ON CONFLICT(base_external_id, quote_external_id) DO UPDATE SET 
+				price = EXCLUDED.price,
+				updated_at = EXCLUDED.updated_at
+		`
+
+		placeholders = make([]string, 0, len(currencies))
+		values       = make([]any, 0, len(currencies)*insertCurrencyColumns)
+	)
+
+	for i, curr := range currencies {
+		index := i * insertCurrencyColumns
+		placeholders = append(placeholders,
+			fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d)",
+				//nolint:mnd
+				index+1, index+2, index+3, index+4, index+5, index+6, index+7),
+		)
+
+		values = append(values,
+			curr.Base,
+			curr.BaseExternalID,
+			curr.Quote,
+			curr.QuoteExternalID,
+			curr.Price,
+			curr.IsInverted,
+			curr.UpdatedAt,
+		)
+	}
+
+	query := fmt.Sprintf(queryTemplate, strings.Join(placeholders, ","))
+
+	res, err := p.db.ExecContext(ctx, query, values...)
+	if err != nil {
+		return fmt.Errorf("exec context: %w", err)
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("rows affected: %w", err)
+	}
+
+	if rows == 0 {
+		return errors.New("no rows affected")
 	}
 
 	return nil
-}
-
-func toDBCurrencies(currencies []exchange.Currency) []*models.Currency {
-	dbCurrencies := make([]*models.Currency, 0, len(currencies))
-
-	for _, v := range currencies {
-		dbCurrencies = append(dbCurrencies, toDBCurrency(v))
-	}
-
-	return dbCurrencies
-}
-
-func toDBCurrency(currency exchange.Currency) *models.Currency {
-	price := types.NewDecimal(decimal.New(0, 0).SetFloat64(float64(currency.Price)))
-
-	return &models.Currency{
-		ID:              currency.ID,
-		Base:            currency.Base.String(),
-		BaseExternalID:  currency.BaseExternalID.String(),
-		Quote:           currency.Quote.String(),
-		QuoteExternalID: currency.QuoteExternalID.String(),
-		Price:           price,
-		IsInverted:      currency.IsInverted,
-		UpdatedAt:       currency.UpdatedAt,
-	}
 }
