94a9c94e98d6c676c25316a917dfac2f01eafcfa
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-05-25 19:57:01 +0300

    unsubscribe user currency

---
+++ internal/domain/user/unsubscribe_currency.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"context"
+	"fmt"
+)
+
+func (u *UserProcessor) UnsubscribeCurrency(ctx context.Context, chatID int64, currencyID int) error {
+	usr, err := u.userByChatID(ctx, chatID)
+	if err != nil {
+		return fmt.Errorf("get user by chat_id: %w", err)
+	}
+
+	if err := u.storage.RemoveUserCurrency(ctx, usr.ID, currencyID); err != nil {
+		if u.storage.IsNotFoundError(err) {
+			return fmt.Errorf("no such currency: %w", err)
+		}
+
+		return fmt.Errorf("remove user currency: %w", err)
+	}
+
+	return nil
+}
